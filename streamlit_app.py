bfe86a7a8646e4b35ccc39afdaa01aaee5476c37
Shaida Shamuri <97875676+FashA9@users.noreply.github.com>
2023-09-01 09:14:53 +1000

    Update streamlit_app.py
    added 3 new menus and add images

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,9 @@
 import streamlit
    
-🥣streamlit.title('My Parents New Healthy Diner')
+streamlit.title('🥣My Parents New Healthy Diner')
 
-🥗streamlit.header('Breakfast Menu')
-🥑streamlit.text('Omega 3 & Blueberry Oatmeal')
-🐔streamlit.text('Kale, Spinach & Rocket Smoothie')
-🍞streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.header('🥗Breakfast Menu')
+streamlit.text('🥑Omega 3 & Blueberry Oatmeal')
+streamlit.text('🐔Kale, Spinach & Rocket Smoothie')
+streamlit.text('🍞Hard-Boiled Free-Range Egg')
 
