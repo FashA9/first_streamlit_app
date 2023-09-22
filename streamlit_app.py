18755a7cb5f37250d8e2102db468144d7924a6cd
Shaida Shamuri <97875676+FashA9@users.noreply.github.com>
2023-09-22 11:38:13 +1000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,12 +1,10 @@
 import streamlit
-   
 streamlit.title('🥣My Parents New Healthy Diner')
 
 streamlit.header('🥗Breakfast Menu')
 streamlit.text('🥑Omega 3 & Blueberry Oatmeal')
 streamlit.text('🐔Kale, Spinach & Rocket Smoothie')
 streamlit.text('🍞Hard-Boiled Free-Range Egg')
-
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 import pandas
@@ -23,9 +21,12 @@ streamlit.dataframe(fruits_to_show)
 #New section to display fruityvice api response
 streamlit.header('Fruityvice Fruit Advice!')
 
+
+
+
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json()) #just writes the data to the screen
+#streamlit.text(fruityvice_response.json()) #just writes the data to the screen
 
 # write your own comment -what does the next line do? 
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
