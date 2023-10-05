6fd6f689fc629465097b6721cc9cc57ee2b06cb7
Shaida Shamuri <97875676+FashA9@users.noreply.github.com>
2023-10-06 09:32:25 +1100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,9 +39,9 @@ fruityvice_response
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("select * from fruit_load_list")
-my_data_row = my_cur.fetchone()
+my_data_rows = my_cur.fetchall()
 streamlit.text("The fruit load list contains")
-streamlit.text(my_data_row)
+streamlit.text(my_data_rows)
 
 
 #create the repeatable code block (called a function)
