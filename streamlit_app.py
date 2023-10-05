5816a5bf11060cfe7b89cf29878b039be0e43898
Shaida Shamuri <97875676+FashA9@users.noreply.github.com>
2023-10-05 17:44:54 +1100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,9 +29,20 @@ fruit_choice = streamlit.text_input('What fruit would you like information about
 streamlit.write('The user entered', fruit_choice)
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-
+fruityvice_response
 #don't run anything past here while we troubleshoot
 #streamlit.stop()
+
+
+
+
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+my_data_row = my_cur.fetchone()
+streamlit.text("Hello from Snowflake:")
+streamlit.text(my_data_row)
+
 
 #create the repeatable code block (called a function)
 #def get_fruityvice_date(this_fruit_choice):
