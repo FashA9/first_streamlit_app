76326e4607e59df75ddebcc51b8e40ae49d6729d
Shaida Shamuri <97875676+FashA9@users.noreply.github.com>
2023-10-06 09:41:11 +1100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,6 +12,7 @@ streamlit.text('🐔Kale, Spinach & Rocket Smoothie')
 streamlit.text('🍞Hard-Boiled Free-Range Egg')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
+
 #import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
@@ -23,16 +24,20 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
 
-#new section to display fruityvice api response
+# New section to display fruityvice api response
 streamlit.header('Fruityvice Fruit Advice!')
 fruit_choice = streamlit.text_input('What fruit would you like information about?', 'Kiwi')
 streamlit.write('The user entered', fruit_choice)
-import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-fruityvice_response
-#don't run anything past here while we troubleshoot
-#streamlit.stop()
 
+#import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+
+
+
+
+
+#don't run anything past here while we troubleshoot
+streamlit.stop()
 
 
 
@@ -40,10 +45,14 @@ my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("select * from fruit_load_list")
 my_data_rows = my_cur.fetchall()
-streamlit.text("The fruit load list contains")
-streamlit.text(my_data_rows)
+streamlit.header("The fruit load list contains:")
+streamlit.dataframe(my_data_rows)
 
+#Allow the end user to add a fruit to the list
 
+streamlit.write('Thanks for adding ', add_my_fruit)
+
+#This will not 
 #create the repeatable code block (called a function)
 #def get_fruityvice_date(this_fruit_choice):
 #   fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
